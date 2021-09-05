da94c1e82572d333569951a7eef6ec48cd1dba7f
isnowheart <isnowheart@icloud.com>
2021-09-05 19:21:11 -0300

    SwaggerSetup Documentation

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerCustomOptions, SwaggerModule } from "@nestjs/swagger";
 import { createConnection } from "typeorm";
 import { AppModule } from "./app.module";
+import { name, description, version } from "../package.json"
 
 async function bootstrap() {
   await createConnection()
   const app = await NestFactory.create(AppModule)
+  const config = new DocumentBuilder().setTitle(name).setDescription(description).setVersion(version).build()
+  const customOptions: SwaggerCustomOptions = { customSiteTitle: 'Users CRUD Documentation' }
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('documentation', app, document, customOptions)
   await app.listen(3000)
 }
 
